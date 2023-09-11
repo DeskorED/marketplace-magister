2b0ff7597b5ddd66cc6533b6ea4d06809fa6a286
DeskorED <rfpfywtdf29@gmail.com>
2023-09-11 21:31:00 +0200

    refactor: modify categories, MarketplaceReact, Search

---
+++ src/constants/categories.js
@@ -0,0 +1,114 @@
+export const categories = [
+    {
+        name: {
+            en: "Laptops and computers",
+            ua: "Ноутбуки та комп'ютери",
+            ru: "Ноутбуки и компьютеры",
+        },
+        items: [
+            {
+                name: "Ноутбук Acer Aspire 5 A515-56G-51Q5 (NX.AT2EU.00M) Pure Silver / Intel Core i5-1135G7 / RAM 8 ГБ / SSD 512 ГБ / nVidia GeForce MX450 / Подсветка клавиатуры",
+                price: {
+                    value: 20000,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content.rozetka.com.ua/goods/images/big/285466617.jpg"
+                }
+            },
+            {
+                name: "Ноутбук ASUS TUF Gaming A15 FA506ICB-HN119 (90NR0667-M00KT0) Graphite Black / AMD Ryzen 5 4600H / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce RTX 3050",
+                price: {
+                    value: 30000,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content1.rozetka.com.ua/goods/images/big/302686477.jpg"
+                }
+            },
+            {
+                name: 'Ноутбук Lenovo IdeaPad Gaming 3 15IHU6 (82K10190RA) Shadow Black / 15.6" IPS Full HD 120 Гц / Intel Core i5-11320H / RAM 16 ГБ / SSD 512 ГБ / nVidia GeForce RTX 3050',
+                price: {
+                    value: 33999,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content.rozetka.com.ua/goods/images/big/334788278.jpg"
+                }
+            },
+
+        ]
+    },
+    {
+        name: {
+            en: "Smartphones, TV and electronics",
+            ua: "Смартфони, ТВ та електроніка",
+            ru: "Смартфоны, ТВ и электроника",
+        },
+        items: [
+            {
+                name: "Мобильный телефон Samsung Galaxy S23 Ultra 12/512GB Green (SM-S918BZGHSEK)",
+                price: {
+                    value: 55799,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content.rozetka.com.ua/goods/images/big/310649358.jpg"
+                }
+            },
+            {
+                name: "Мобильный телефон Apple iPhone 13 128GB Midnight (MLPF3HU/A)",
+                price: {
+                    value: 33499,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content1.rozetka.com.ua/goods/images/big/221214139.jpg"
+                }
+            },
+            {
+                name: 'Мобильный телефон Samsung Galaxy A24 6/128GB Black (SM-A245FZKVSEK)',
+                price: {
+                    value: 8999,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content.rozetka.com.ua/goods/images/big/328132324.jpg"
+                }
+            },
+
+        ]
+    },
+    {
+        name: {
+            en: "Household appliances",
+            ua: "Побутова техніка",
+            ru: "Бытовая техника",
+        },
+        items: [
+            {
+                name: "Холодильник SAMSUNG RB33J3200SA/UA",
+                price: {
+                    value: 22799,
+                    unit: "UAH",
+                    description: "...",
+                    img:"https://content.rozetka.com.ua/goods/images/big/359825406.jpg"
+                }
+            },
+            {
+                name: "Стиральная машина узкая WHIRLPOOL WRBSB 6228 B UA",
+                price: {
+                    value: 13222,
+                    unit: "UAH",
+                    description: "...",
+                    img:"https://content.rozetka.com.ua/goods/images/big/286602036.jpg"
+                }
+            },
+            {
+                name: 'Фен Philips series 3000 BHD351/10',
+                price: {
+                    value: 1199,
+                    unit: "UAH",
+                    description: "...",
+                    img: "https://content2.rozetka.com.ua/goods/images/big/359265112.png"
+                }
+            },
+
+        ]
+    },
+
+]
--- src/pages/MarketplaceReact/MarketplaceReact.jsx
+++ src/pages/MarketplaceReact/MarketplaceReact.jsx
@@ -1,3 +1,5 @@
+import { categories } from "../../constants/categories"
+
 import { Header } from "./components/Header"
 
 import "./style.scss"
@@ -6,7 +8,39 @@ export const MarketplaceReact = () => {
     return (
         <div className="market-place-page">
             <Header />
-            Marketplace Page
+            <div className="categories-block">
+                {
+                    categories.map(
+                        category => {
+                            return (
+                                <div className="categories__item">
+                                    <div className="categories__name">
+                                        <div className="name__value">
+                                            {
+                                                category?.name?.ua
+                                            }
+                                        </div>
+                                        <hr className="name__hr" />
+                                    </div>
+                                    <div className="categories__products">
+                                        {
+                                            category?.items?.map(
+                                                (product) => {
+                                                    return (
+                                                        <div>
+                                                            {product?.name}
+                                                        </div>
+                                                    )
+                                                }
+                                            )
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        }
+                    )
+                }
+            </div>
         </div>
     )
 }
